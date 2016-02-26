7a1df60d2ed5347877de28c110f481f522da7b78
spike <morndragon@126.com>
2016-02-26 17:49:15 +0800

    first commit

---
+++ app/src/main/java/me/chunyu/spike/wcl_flip_anim_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package me.chunyu.spike.wcl_flip_anim_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
