e1590a7905819ac45bc2d643741464705e7eda56
spike <morndragon@126.com>
2016-02-27 18:27:58 +0800

    完成全部动画效果

--- app/src/main/java/me/chunyu/spike/wcl_flip_anim_demo/MainActivity.java
+++ app/src/main/java/me/chunyu/spike/wcl_flip_anim_demo/MainActivity.java
@@ -1,13 +1,80 @@
 package me.chunyu.spike.wcl_flip_anim_demo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.main_fl_container) FrameLayout mFlContainer;
+    @Bind(R.id.main_fl_card_back) FrameLayout mFlCardBack;
+    @Bind(R.id.main_fl_card_front) FrameLayout mFlCardFront;
+
+    private AnimatorSet mRightOutSet; // 右出动画
+    private AnimatorSet mLeftInSet; // 左入动画
+
+    private boolean mIsShowBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setAnimators();
+        setCameraDistance();
+    }
+
+    // 设置动画
+    private void setAnimators() {
+        mRightOutSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.anim_out);
+        mLeftInSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.anim_in);
+
+        // 设置点击事件
+        mRightOutSet.addListener(new AnimatorListenerAdapter() {
+            @Override public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                mFlContainer.setClickable(false);
+            }
+        });
+        mLeftInSet.addListener(new AnimatorListenerAdapter() {
+            @Override public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                mFlContainer.setClickable(true);
+            }
+        });
+    }
+
+    // 改变视角距离, 贴近屏幕
+    private void setCameraDistance() {
+        int distance = 16000;
+        float scale = getResources().getDisplayMetrics().density * distance;
+        mFlCardFront.setCameraDistance(scale);
+        mFlCardBack.setCameraDistance(scale);
+    }
+
+    // 翻转卡片
+    public void flipCard(View view) {
+        if (!mIsShowBack) {
+            mRightOutSet.setTarget(mFlCardFront);
+            mLeftInSet.setTarget(mFlCardBack);
+            mRightOutSet.start();
+            mLeftInSet.start();
+            mIsShowBack = true;
+        } else {
+            mRightOutSet.setTarget(mFlCardBack);
+            mLeftInSet.setTarget(mFlCardFront);
+            mRightOutSet.start();
+            mLeftInSet.start();
+            mIsShowBack = false;
+        }
     }
 }
